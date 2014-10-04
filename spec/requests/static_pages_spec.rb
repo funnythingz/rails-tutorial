3e51064f2ac4961f1fb41013809f6ce120bdc4ac
funnythingz <funnythingz69+github@gmail.com>
2014-10-04 21:39:24 +0900

    Fixed test for `Home`, `Help`

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,25 +4,36 @@ RSpec.describe "StaticPages", :type => :request do
 
   describe "GET /static_pages" do
 
-    it "access to `/static_pages/home` responds successfully with an HTTP 200 status code" do
-
-      get "/static_pages/home"
-      expect(response).to be_success
-      expect(response).to have_http_status(200)
-
-    end
-
-    it "access to `/static_pages/help` responds successfully with an HTTP 200 status code" do
-      get "/static_pages/help"
-      expect(response).to be_success
-      expect(response).to have_http_status(200)
-    end
-
     describe "/home" do
-      it "should have the content `Sample App`" do
-        visit "/static_pages/home"
-        expect(page).to have_content('Sample App')
+
+      it "access to `/static_pages/home` responds successfully with an HTTP 200 status code" do
+
+        get "/static_pages/home"
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+
       end
+
+      it "should have the content `Home`" do
+        visit "/static_pages/home"
+        expect(page).to have_content('Home')
+      end
+
+    end
+
+    describe "/help" do
+
+      it "access to `/static_pages/help` responds successfully with an HTTP 200 status code" do
+        get "/static_pages/help"
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+      end
+
+      it "should have the content `Help`" do
+        visit "/static_pages/help"
+        expect(page).to have_content('Help')
+      end
+
     end
 
   end
