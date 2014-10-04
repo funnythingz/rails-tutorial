c82097de66c0e7fca6716a0c5e654f1a39258894
funnythingz <funnythingz69+github@gmail.com>
2014-10-04 21:43:14 +0900

    added test code of `/static_pages/about`

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe "StaticPages", :type => :request do
   describe "GET /static_pages" do
 
     describe "/home" do
-
       it "access to `/static_pages/home` responds successfully with an HTTP 200 status code" do
 
         get "/static_pages/home"
@@ -18,11 +17,9 @@ RSpec.describe "StaticPages", :type => :request do
         visit "/static_pages/home"
         expect(page).to have_content('Home')
       end
-
     end
 
     describe "/help" do
-
       it "access to `/static_pages/help` responds successfully with an HTTP 200 status code" do
         get "/static_pages/help"
         expect(response).to be_success
@@ -33,7 +30,19 @@ RSpec.describe "StaticPages", :type => :request do
         visit "/static_pages/help"
         expect(page).to have_content('Help')
       end
+    end
 
+    describe "/about" do
+      it "access to `/static_pages/about` responds successfully with an HTTP 200 status code" do
+        get "/static_pages/about"
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+      end
+
+      it "should have the content `About me`" do
+        visit "/static_pages/about"
+        expect(page).to have_content('About me')
+      end
     end
 
   end
